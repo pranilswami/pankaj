c6613a8814d3bc869058af596a0f81ad8378625b
Pranil Swami <100079926+pranilswami@users.noreply.github.com>
2023-05-31 21:12:42 +0530

    Add files via upload

---
+++ JAVA/java3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class java3 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Computer Will Guess : \nEnter 'higher' if computer goes too low and enter 'lower' if computer goes too high\n\n");
+        System.out.println("Start the Game : \n");
+        System.out.print("Enter Your Number: ");
+        int User_Number = scan.nextInt();
+        int RandomNumber=0,low=1,high=101;
+        String help="p";
+        do
+        {
+            RandomNumber = ThreadLocalRandom.current().nextInt(low,high);
+            System.out.println("\nIs this your number : "+RandomNumber);
+            if(RandomNumber>User_Number)
+            {
+                high = RandomNumber;
+            }
+            else if(RandomNumber<User_Number)
+            {
+                low = RandomNumber;
+                
+            }
+            System.out.print("Help : ");
+            help = scan.next();
+        }while(!help.equals("correct"));
+
+        System.out.println("\nThank You for helping me!!!");
+    }
+}
