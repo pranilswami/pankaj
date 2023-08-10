fba023e93c7c9e918596f79de2ac9cba6383e10b
Pranil Swami <100079926+pranilswami@users.noreply.github.com>
2023-08-10 13:41:13 +0530

    Delete EmailApp.java

--- JAVA/EmailApp.java
+++
@@ -1,9 +0,0 @@
-package emailapp;
-
-public class EmailApp {
-    public static void main(String[] args) {
-        Email e1 = new Email("Pranil", "Swami");
-        System.out.println(e1.showinfo());
-        
-    }
-}
